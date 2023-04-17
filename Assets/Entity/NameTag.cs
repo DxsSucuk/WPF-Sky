9b0a7f937e71f1f9b539d6e87648c79fcc87ac03
Presti <dxssucuk@hotmail.com>
2023-04-17 18:56:54 +0200

    Nametags YIPEEEE!

---
+++ Assets/Entity/NameTag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class NameTag : MonoBehaviourPun
+{
+    [SerializeField]
+    private TMP_Text NametagText;
+
+    private Transform mainCameraTransform;
+
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+        if (photonView.IsMine)
+        {
+            return;
+        }
+
+        SetName();
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + mainCameraTransform.rotation * Vector3.forward,
+            mainCameraTransform.rotation * Vector3.up);
+    }
+
+    private void SetName() => NametagText.text = photonView.Owner.NickName;
+}
